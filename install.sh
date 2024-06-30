bbb3348920d5307ba6065ccf987c8d96fd4cad17
falk <falk@bummelletzter.com>
2024-06-30 16:20:41 +0200

    initial upload

--- install.sh
+++ install.sh
@@ -36,7 +36,7 @@ echo $password_app | su - "$APP_USER" -c "cd ~ &"
 echo $password_app | su - "$APP_USER" -c "virtualenv birdshome &"
 echo $password_app | su - "$APP_USER" -c "source birdshome/bin/activate &"
 echo $password_app | su - "$APP_USER" -c "git clone https://github.com/fichtlandsachs/birdshome2.git &"
-echo $password_inst | su - "$INSTALL_USER" -c "cp /home/$APP_USER/birdshome2 /etc/birdshome &"
+echo $password_inst | su - "$INSTALL_USER" -c "cp  /etc/birdshome /home/$APP_USER/birdshome2&"
 
 # install required packages
 #pip3 install flask werkzeug flask_RESTful flask-SQLAlchemy mpld3 pandas pyaudio
@@ -53,33 +53,33 @@ fi
 if id -u $SMB_USER >/dev/null 2>&1; then
   echo "user $SMB_USER exists"
 else
-  echo $password_inst | su - "$INSTALL_USER" -c "useradd -s /bin/false $SMB_USER"
-  echo $password_inst | su - "$INSTALL_USER" -c "smbpasswd -a $SMB_USER"
+  echo $password_inst | su - "$INSTALL_USER" -c "sudo useradd -s /bin/false $SMB_USER"
+  echo $password_inst | su - "$INSTALL_USER" -c "sudo smbpasswd -a $SMB_USER"
   echo "user $SMB_USER created"
 fi
 
 if [ ! -d "/etc/birdshome" ]; then
-  echo $password_inst | su - "$INSTALL_USER" -c "mkdir /etc/birdshome"
+  echo $password_inst | su - "$INSTALL_USER" -c "sudo mkdir /etc/birdshome"
 fi
-echo $password_inst | su - "$INSTALL_USER" -c "chown -R $APP_USER:$APP_USER $FLD_BIRDSHOME_ROOT"
+echo $password_inst | su - "$INSTALL_USER" -c "sudo chown -R $APP_USER:$APP_USER $FLD_BIRDSHOME_ROOT"
 
 if [ ! -d $FLD_BIRDSHOME ]; then
-  echo $password_inst | su - "$INSTALL_USER" -c "mkdir $FLD_BIRDSHOME"
+  echo $password_inst | su - "$INSTALL_USER" -c "sudo mkdir $FLD_BIRDSHOME"
 fi
 if [ ! -d $FLD_BIRDSHOME'/static' ]; then
-  echo $password_inst | su - "$INSTALL_USER" -c "mkdir $FLD_BIRDSHOME'/static'"
+  echo $password_inst | su - "$INSTALL_USER" -c "sudo mkdir $FLD_BIRDSHOME'/static'"
 fi
 if [ ! -d $FLD_BIRDSHOME'/static/media' ]; then
-  echo $password_inst | su - "$INSTALL_USER" -c "mkdir $FLD_BIRDSHOME'/static/media'"
+  echo $password_inst | su - "$INSTALL_USER" -c "sudo mkdir $FLD_BIRDSHOME'/static/media'"
 fi
 if [ ! -d $FLD_BIRDSHOME'/static/media/photos' ]; then
-  echo $password_inst | su - "$INSTALL_USER" -c "mkdir $FLD_BIRDSHOME'/static/media/photos'"
+  echo $password_inst | su - "$INSTALL_USER" -c "sudo mkdir $FLD_BIRDSHOME'/static/media/photos'"
 fi
 if [ ! -d $FLD_BIRDSHOME'/static/media/videos' ]; then
-  echo $password_inst | su - "$INSTALL_USER" -c "mkdir $FLD_BIRDSHOME'/static/media/videos'"
+  echo $password_inst | su - "$INSTALL_USER" -c "sudo mkdir $FLD_BIRDSHOME'/static/media/videos'"
 fi
 if [ ! -d $FLD_BIRDSHOME'/static/media/charts' ]; then
-  echo $password_inst | su - "$INSTALL_USER" -c "mkdir $FLD_BIRDSHOME'/static/media/charts'"
+  echo $password_inst | su - "$INSTALL_USER" -c "sudo  mkdir $FLD_BIRDSHOME'/static/media/charts'"
 fi
 
 # Überprüfen, ob die Datei existiert
@@ -89,72 +89,72 @@ if [ ! -f $SMB_CONF ]; then
 fi
 
 echo 'start to configure samba share'
-echo $password_inst | su - "$INSTALL_USER" -c "cp $SMB_CONF $SMB_CONF.original"
+echo $password_inst | su - "$INSTALL_USER" -c "sudo cp $SMB_CONF $SMB_CONF.original"
 echo 'samba.conf saved to smd.conf.original'
-echo $password_inst | su - "$INSTALL_USER" -c "cp $SMB_CONF $SMB_CONF_TMP"
-echo $password_inst | su - "$INSTALL_USER" -c "sed -i 's/workgroup = .*$/workgroup = smb/' $SMB_CONF_TMP"
+echo $password_inst | su - "$INSTALL_USER" -c "sudo cp $SMB_CONF $SMB_CONF_TMP"
+echo $password_inst | su - "$INSTALL_USER" -c "sudo sed -i 's/workgroup = .*$/workgroup = smb/' $SMB_CONF_TMP"
 echo 'changed workgroup to smb'
-echo $password_inst | su - "$INSTALL_USER" -c "sed -i 's/security = .*$/security = user/' $SMB_CONF_TMP"
+echo $password_inst | su - "$INSTALL_USER" -c "sudo sed -i 's/security = .*$/security = user/' $SMB_CONF_TMP"
 echo 'changed security to user'
-echo $password_inst | su - "$INSTALL_USER" -c "sed -i 's/map to guest = .*$/map to guest = never/' $SMB_CONF_TMP"
+echo $password_inst | su - "$INSTALL_USER" -c "sudo sed -i 's/map to guest = .*$/map to guest = never/' $SMB_CONF_TMP"
 echo 'changed map to guest to never'
 
 if ! grep -q '[bird_media]' $SMB_CONF_TMP; then
-  echo $password_inst | su - "$INSTALL_USER" -c "sed -i -e '$a\' -e '[bird_media]' $SMB_CONF_TMP"
+  echo $password_inst | su - "$INSTALL_USER" -c "sudo sed -i -e '$a\' -e '[bird_media]' $SMB_CONF_TMP"
 fi
 if ! grep -A 100 "^\[bird_media\]" "$SMB_CONF_TMP" | awk '/^\[/{exit} /'"path"'/' | grep -q "path"; then
-  echo $password_inst | su - "$INSTALL_USER" -c "sed -i '/^\[bird_media\]/a path='$FLD_BIRDSHOME_MEDIA $SMB_CONF_TMP"
+  echo $password_inst | su - "$INSTALL_USER" -c "sudo sed -i '/^\[bird_media\]/a path='$FLD_BIRDSHOME_MEDIA $SMB_CONF_TMP"
 else
-  echo $password_inst | su - "$INSTALL_USER" -c "sed -i '/^\[bird_media\]/s path= .*$/path ='$FLD_BIRDSHOME_MEDIA $SMB_CONF_TMP"
+  echo $password_inst | su - "$INSTALL_USER" -c "sudo sed -i '/^\[bird_media\]/s path= .*$/path ='$FLD_BIRDSHOME_MEDIA $SMB_CONF_TMP"
 fi
 if ! grep -A 100 "^\[bird_media\]" "$SMB_CONF_TMP" | awk '/^\[/{exit} /'"public"'/' | grep -q "public"; then
-  echo $password_inst | su - "$INSTALL_USER" -c "sed -i '/^\[bird_media\]/a public = yes' $SMB_CONF_TMP"
+  echo $password_inst | su - "$INSTALL_USER" -c "sudo sed -i '/^\[bird_media\]/a public = yes' $SMB_CONF_TMP"
 else
-  echo $password_inst | su - "$INSTALL_USER" -c "sed -i '/^\[bird_media\]/s public = .*$/public = yes' $SMB_CONF_TMP"
+  echo $password_inst | su - "$INSTALL_USER" -c "sudo sed -i '/^\[bird_media\]/s public = .*$/public = yes' $SMB_CONF_TMP"
 fi
 
 if ! grep -A 100 "^\[bird_media\]" "$SMB_CONF_TMP" | awk '/^\[/{exit} /'"writable"'/' | grep -q "writable"; then
-  echo $password_inst | su - "$INSTALL_USER" -c "sed -i '/^\[bird_media\]/a writable = yes' $SMB_CONF_TMP"
+  echo $password_inst | su - "$INSTALL_USER" -c "sudo sed -i '/^\[bird_media\]/a writable = yes' $SMB_CONF_TMP"
 else
-  echo $password_inst | su - "$INSTALL_USER" -c "sed -i '/^\[bird_media\]/s writable = .*$/writable = yes' $SMB_CONF_TMP"
+  echo $password_inst | su - "$INSTALL_USER" -c "sudo sed -i '/^\[bird_media\]/s writable = .*$/writable = yes' $SMB_CONF_TMP"
 fi
 
 if ! grep -A 100 "^\[bird_media\]" "$SMB_CONF_TMP" | awk '/^\[/{exit} /'"comment"'/' | grep -q "comment"; then
-echo $password_inst | su - "$INSTALL_USER" -c "sed -i '/^\[bird_media\]/a comment = video share' $SMB_CONF_TMP"
+echo $password_inst | su - "$INSTALL_USER" -c "sudo sed -i '/^\[bird_media\]/a comment = video share' $SMB_CONF_TMP"
 else
-  echo $password_inst | su - "$INSTALL_USER" -c "sed -i '/^\[bird_media\]/s comment = .*$/comment = video share' $SMB_CONF_TMP"
+  echo $password_inst | su - "$INSTALL_USER" -c "sudo sed -i '/^\[bird_media\]/s comment = .*$/comment = video share' $SMB_CONF_TMP"
 fi
 
 if ! grep -A 100 "^\[bird_media\]" "$SMB_CONF_TMP" | awk '/^\[/{exit} /'"printable"'/' | grep -q "printable"; then
-  echo $password_inst | su - "$INSTALL_USER" -c "sed -i '/^\[bird_media\]/a printable = no' $SMB_CONF_TMP"
+  echo $password_inst | su - "$INSTALL_USER" -c "sudo sed -i '/^\[bird_media\]/a printable = no' $SMB_CONF_TMP"
 else
-  echo $password_inst | su - "$INSTALL_USER" -c "sed -i '/^\[bird_media\]/s printable = .*$/printable = no' $SMB_CONF_TMP"
+  echo $password_inst | su - "$INSTALL_USER" -c "sudo sed -i '/^\[bird_media\]/s printable = .*$/printable = no' $SMB_CONF_TMP"
 fi
 
 if ! grep -A 100 "^\[bird_media\]" "$SMB_CONF_TMP" | awk '/^\[/{exit} /'"guest ok"'/' | grep -q "guest ok"; then
-  echo $password_inst | su - "$INSTALL_USER" -c "sed -i '/^\[bird_media\]/a guest ok = no' $SMB_CONF_TMP"
+  echo $password_inst | su - "$INSTALL_USER" -c "sudo sed -i '/^\[bird_media\]/a guest ok = no' $SMB_CONF_TMP"
 else
-  echo $password_inst | su - "$INSTALL_USER" -c "sed -i '/^\[bird_media\]/s guest ok = .*$/guest ok = no' $SMB_CONF_TMP"
+  echo $password_inst | su - "$INSTALL_USER" -c "sudo sed -i '/^\[bird_media\]/s guest ok = .*$/guest ok = no' $SMB_CONF_TMP"
 fi
 if ! grep -A 100 "^\[bird_media\]" "$SMB_CONF_TMP" | awk '/^\[/{exit} /'"valid users"'/' | grep -q "valid users"; then
-  echo $password_inst | su - "$INSTALL_USER" -c "sed -i '/^\[bird_media\]/a valid users = '$SMB_USER', @'$SMB_USER $SMB_CONF_TMP"
+  echo $password_inst | su - "$INSTALL_USER" -c "sudo sed -i '/^\[bird_media\]/a valid users = '$SMB_USER', @'$SMB_USER $SMB_CONF_TMP"
 else
-  echo $password_inst | su - "$INSTALL_USER" -c "sed -i '/^\[bird_media\]/s valid users = .*$/valid users = ${SMB_USER}, @${SMB_USER}' $SMB_CONF_TMP"
+  echo $password_inst | su - "$INSTALL_USER" -c "sudo sed -i '/^\[bird_media\]/s valid users = .*$/valid users = ${SMB_USER}, @${SMB_USER}' $SMB_CONF_TMP"
 fi
 if ! grep -A 100 "^\[bird_media\]" "$SMB_CONF_TMP" | awk '/^\[/{exit} /'"write list"'/' | grep -q "write list"; then
-  echo $password_inst | su - "$INSTALL_USER" -c "sed -i '/^\[bird_media\]/a write list = '$SMB_USER', @'$SMB_USER $SMB_CONF_TMP"
+  echo $password_inst | su - "$INSTALL_USER" -c "sudo sed -i '/^\[bird_media\]/a write list = '$SMB_USER', @'$SMB_USER $SMB_CONF_TMP"
 else
-  echo $password_inst | su - "$INSTALL_USER" -c "sed -i '/^\[bird_media\]/s write list = .*$/write list = ${SMB_USER}, @${SMB_USER}' $SMB_CONF_TMP"
+  echo $password_inst | su - "$INSTALL_USER" -c "sudo sed -i '/^\[bird_media\]/s write list = .*$/write list = ${SMB_USER}, @${SMB_USER}' $SMB_CONF_TMP"
 fi
 if ! grep -A 100 "^\[bird_media\]" "$SMB_CONF_TMP" | awk '/^\[/{exit} /'"create mask"'/' | grep -q "create mask"; then
-  echo $password_inst | su - "$INSTALL_USER" -c "sed -i '/^\[bird_media\]/a create mask = 0600' $SMB_CONF_TMP"
+  echo $password_inst | su - "$INSTALL_USER" -c "sudo sed -i '/^\[bird_media\]/a create mask = 0600' $SMB_CONF_TMP"
 else
-  echo $password_inst | su - "$INSTALL_USER" -c "sed -i '/^\[bird_media\]/s create mask = .*$/create mask = 0600' $SMB_CONF_TMP"
+  echo $password_inst | su - "$INSTALL_USER" -c "sudo sed -i '/^\[bird_media\]/s create mask = .*$/create mask = 0600' $SMB_CONF_TMP"
 fi
 if ! grep -A 100 "^\[bird_media\]" "$SMB_CONF_TMP" | awk '/^\[/{exit} /'"directory mask "'/' | grep -q "directory mask "; then
-  echo $password_inst | su - "$INSTALL_USER" -c "sed -i '/^\[bird_media\]/a directory mask = 0700' $SMB_CONF_TMP"
+  echo $password_inst | su - "$INSTALL_USER" -c "sudo sed -i '/^\[bird_media\]/a directory mask = 0700' $SMB_CONF_TMP"
 else
-  echo $password_inst | su - "$INSTALL_USER" -c "sed -i '/^\[bird_media\]/s directory mask = .*$/directory mask  = 0700' $SMB_CONF_TMP"
+  echo $password_inst | su - "$INSTALL_USER" -c "sudo sed -i '/^\[bird_media\]/s directory mask = .*$/directory mask  = 0700' $SMB_CONF_TMP"
 fi
 
 
