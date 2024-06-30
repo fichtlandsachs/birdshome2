5180e50323797a595a566d1d9c5f6beba1d3a601
falk <falk@bummelletzter.com>
2024-06-30 22:37:07 +0200

    initial upload

--- birds_dev.sh
+++ birds_dev.sh
@@ -1,4 +1,4 @@
 #source env/bin/activate
-/bin/bash -c  "source ~/birdshome/bin/activate; exec /bin/bash -i"
+/bin/bash -c  "source /home/birdie/birdshome/bin/activate; exec /bin/bash -i"
 gunicorn3 --bind 0.0.0.0:5000 --threads 5 -w 1 --timeout 120 app:app
 deactivate
