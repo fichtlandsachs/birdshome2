92e3c5d6f77f8a49dc6f20f4073da8620af1d746
falk <falk@bummelletzter.com>
2024-07-12 17:20:03 +0200

    initial upload

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ while true; do
       3>&1 1>&2 2>&3)
         case $OPTION_INSTALL_USER in
           1)
-          INSTALL_USER=$(whiptail --title "Installation user" --inputbox "Installation User ID:" 10 60)
+          INSTALL_USER=$(whiptail --title "Installation user" --inputbox "Installation User ID:" 10 60 [ "" ])
             if [ -n "$INSTALL_USER" ]; then
               whiptail --title "Installation user" --msgbox "Please provide a valid user"
             else
