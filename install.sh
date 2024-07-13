e4549836a75e72c64a529aace5d447a6b2d123e4
falk <falk@bummelletzter.com>
2024-07-13 15:36:48 +0200

    initial upload

--- install.sh
+++ install.sh
@@ -154,8 +154,8 @@ sleep 2s
 if id -u $SMB_USER >/dev/null 2>&1; then
   echo "user $SMB_USER exists"
 else
-echo "$password_inst" | su - "$INSTALL_USER" -c "sudo useradd -s /bin/false $SMB_USER"
-  su - "$INSTALL_USER" -c "echo '$APP_USER:$password_app' | sudo chpasswd"
+  echo "$password_inst" | su - "$INSTALL_USER" -c "sudo useradd -s /bin/false $SMB_USER"
+  echo "$password_inst" | su - "$INSTALL_USER" -c "echo '$APP_USER:$password_app' | sudo chpasswd"
   echo "user $SMB_USER created"
 fi
 echo "$password_inst" | su - "$INSTALL_USER" -c "sudo chown -R $APP_USER:$APP_USER $FLD_BIRDSHOME_ROOT"
